a1f5f39a77bda36ecfc8fb9ac901a691aa31ab44
Djeremai <Djeremai@192.168.227.99>
2012-05-08 15:22:24 +0200

    annule les changements du tests malencontreusement commités(...)

--- ProjetGenieLogiciel/test/structurereseau/LigneTest.java
+++ ProjetGenieLogiciel/test/structurereseau/LigneTest.java
@@ -26,7 +26,7 @@ public class LigneTest {
         Ligne l1 = Ligne.getlLignes().get(1);
         System.out.println("l1 : "+l1.getNumero());
         l1.getLfrag().get(2).setIncident(true);
-        //incident sur la station Réaumur Sébastopol et Barbès
+        //incident sur la station Réaumur Sébastopol
         Station.getLstation().get("Réaumur Sébastopol").setIncident(true);
     }
    
@@ -40,8 +40,7 @@ public class LigneTest {
         String arrivee = "Place d'Italie";
         ArrayList<Fragment> expResult = new ArrayList<Fragment>();
         expResult.add(new Fragment(1, "La Défense", "Charles de Gaulle", 5, false));
-        expResult.add(new Fragment(1, "Charles de Gaulle", "Châtelet", 2, false));
-        expResult.add(new Fragment(4, "Châtelet", "Montparnasse", 13, false));
+        expResult.add(new Fragment(6, "Charles de Gaulle", "Montparnasse", 15, false));
         expResult.add(new Fragment(6, "Montparnasse", "Place d'Italie", 5, false));
         ArrayList result = Ligne.fastestWay(depart, arrivee);
         assertEquals(expResult.toString(), result.toString());
